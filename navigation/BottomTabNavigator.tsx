9aedbe5be5c9b465dd5f826fca2107a6bcc5d977
oliverkill <killoliver08@gmail.com>
2020-12-07 14:46:32 +0200

    Kodu kuval on mitu AlbumCategory komponenti, mis omakorda sisaldavad Album komponente [Delivers #175848829]

--- navigation/BottomTabNavigator.tsx
+++ navigation/BottomTabNavigator.tsx
@@ -50,8 +50,6 @@ export default function BottomTabNavigator() {
   );
 }
 
-// You can explore the built-in icon families and icons on the web at:
-// https://icons.expo.fyi/
 function TabBarIcon(props: { name: string; color: string }) {
   return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
 }
